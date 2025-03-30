861cfa2af2b59319af8e55aee221eb1894be74de
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 17:29:21 -0500

    feat: :sparkles: `Ejercicio 7.`
    //     /* Retornar la cadena invertida. Por ejemplo, si la entrada es "Hola", la salida debe ser "aloH". */
    
    https://www.youtube.com/watch?v=VA77JQ2HLs8&t=1s

---
+++ Ejercicio7.js
@@ -0,0 +1,14 @@
+// // EJERCICIO 7
+// function invertirCadena(cadena) {
+//     /* Retornar la cadena invertida. Por ejemplo, si la entrada es "Hola", la salida debe ser "aloH". */
+//     // NOTA: Vas a tener que googlear como invertir una string en javascript ;)
+
+//     // Escribi tu codigo aca abajo:
+
+// }
+
+let frase = "Hola"
+
+let reversa = frase.split("").reverse().join("")
+
+console.log(reversa)
