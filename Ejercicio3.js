00acf0fe2bc5542e7b2fb8a746a6e6bbadc18cff
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 15:43:44 -0500

    feat: :sparkles: `Ejercicio 3.`
    /* Retornar el promedio entre los 4 argumentos pasados por parametro */
    
    https://www.youtube.com/watch?v=VA77JQ2HLs8&t=1s

---
+++ Ejercicio3.js
@@ -0,0 +1,13 @@
+// // EJERCICIO 3
+
+// function promedioEntreCuatro(num1, num2, num3, num4) {
+//     /* Retornar el promedio entre los 4 argumentos pasados por parametro */
+//     // Escribi tu codigo aca abajo:
+
+// };
+
+function promedioEntreCuatro(num1, num2, num3, num4) {
+    return (num1 + num2 + num3 + num4)/promedioEntreCuatro.length
+}
+
+console.log(promedioEntreCuatro(2,2,2,2))
