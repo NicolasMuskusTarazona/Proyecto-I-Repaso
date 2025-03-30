4d51d6cb052396ade3e9eb549d6e9adc6093b1d3
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 15:45:32 -0500

    feat: :sparkles: `Ejercicio 4.`
    /* Retornar el cuadrado del número pasado por parámetro. */
    
    https://www.youtube.com/watch?v=VA77JQ2HLs8&t=1s

---
+++ Ejercicio4.js
@@ -0,0 +1,13 @@
+// // EJERCICIO 4
+// function cuadradoDeUnNumero(num) {
+//     /* Retornar el cuadrado del número pasado por parámetro. */
+//     // NOTA: Podes googlear la formula para elevar un numero al cuadrado ;)
+
+//     // Escribi tu codigo aca abajo:
+
+//}
+
+function cuadradoDeUnNumero(num) {
+    return Math.pow(num,2)
+}
+console.log(cuadradoDeUnNumero(7))
