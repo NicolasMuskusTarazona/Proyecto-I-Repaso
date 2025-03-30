4f249c9ac849f1508330ec008ec296b4dd976ab2
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 15:40:45 -0500

    feat: :sparkles: `Ejercicio 2.`
    /* Retornar el promedio entre los dos argumentos (num1 y num2). */
    
    https://www.youtube.com/watch?v=VA77JQ2HLs8&t=1s

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+// function dividirPorTres(num) {
+//     /* Retornar el argumento num dividido por 3. */
+//     // Escribi tu codigo aca abajo
+
+// };
+
+function dividirPorTres(num) {
+    return num/3
+}
+console.log(dividirPorTres(3))
---
+++ Ejercicio2.js
@@ -0,0 +1,13 @@
+// // EJERCICIO 2
+
+// function promedioEntreDos(num1, num2) {
+//     /* Retornar el promedio entre los dos argumentos (num1 y num2). */
+//     // Escribi tu codigo aca abajo:
+
+// };
+
+function promedioEntreDos(num1, num2) {
+    return (num1 + num2)/promedioEntreDos.length
+}
+
+console.log(promedioEntreDos(2,2))
